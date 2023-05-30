7ab3bca78929c59039e85cf85fa180e4274c4b2a
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-05-30 14:07:45 +0100

    added a header

---
+++ Components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header () {
+    return (
+        <>
+        <h1>Just News</h1>
+        <p>No blues, no tunes, no snooze, no boos... Just News</p>
+        </>
+    )
+}
