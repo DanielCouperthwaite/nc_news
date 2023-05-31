ac962b83a015dbcd83831ba14abb294f13545a28
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-05-31 11:52:12 +0100

    adding dates to comments and articles

--- Components/ArticleComments.jsx
+++ Components/ArticleComments.jsx
@@ -1,10 +1,12 @@
+import moment from 'moment'
+
 export default function ArticleComments ({currentComments}) {
     return (
         <>
-             {currentComments.map(({body, author, votes, comment_id}) => {
+             {currentComments.map(({body, author, votes, comment_id, created_at}) => {
                     return <li key={comment_id}>
                         <p>{body}</p>
-                        <p>Submitted by {author}, votes: {votes}</p>
+                        <p>Submitted by {author} on {moment(`${created_at}`).format(`Do MMMM YYYY`)}{` `},  votes: {votes}</p>
                     </li>
                 })}
         </>
--- Components/ArticleContent.jsx
+++ Components/ArticleContent.jsx
@@ -1,9 +1,11 @@
+import moment from 'moment'
+
 export default function ArticleContent ({currentArticle}) {
     return (
         <>
             
             <h2>{currentArticle.title}</h2>
-            {/* <p>🕓 {moment().format(currentArticle.created_at, "DD MM YYYY hh:mm:ss")}</p> */}
+            <h3>🕓 {moment(`${currentArticle.created_at}`).format(`Do MMMM YYYY`)}{` `} </h3>
             <p>By {currentArticle.author}</p>
             <p>Votes: {currentArticle.votes}</p>
             <img src={currentArticle.article_img_url} alt={currentArticle.title}></img>
