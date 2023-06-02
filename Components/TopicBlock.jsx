87663cf872ada355bc548394c91724eb11f5072d
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-02 16:42:05 +0100

    styling changes

--- Components/TopicBlock.jsx
+++ Components/TopicBlock.jsx
@@ -5,12 +5,12 @@ export default function ({currentTopics}) {
     return (
         <>
         <h2>Topics:</h2>
-        <p>Just what you need...</p>
+        
         <ul className="article-block">>
         {currentTopics.map(({slug}) => {
                     return (
-                    <ul className="article-block">
-                    <li key={slug}>
+                    <ul className="topic">
+                    <li className="topic li" key={slug}>
                         <Link to={`/articlesByTopic/${slug}`}><h2>{slug}</h2></Link>
                     </li>
                     </ul>
